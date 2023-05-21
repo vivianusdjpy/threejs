1ba59430081cc4af2571172060cab5bb25330127
VIVIAN NJAU <viviannjau@users.noreply.github.com>
2023-05-21 16:28:41 +0200

    routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import cors from cors;
+import cors from 'cors';
+
+
+import dalleRoutes from './routes/dalle.routes.js';
 
 dotenv.config();
 
@@ -8,6 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json( {limig: "50mb" }))
 
+app.use('./api/v1/dalle', dalleRoutes);
+
 app.get('/', (req, res) => {
-    res.status(200).json({ message: "Hello world"})
+    res.status(200).json({ message: "Hello from DALL.E"})
 })
+
+app.listen(8080, () => console.log('Server has started on port 8080'))
