c0827479c5ee6e122964bfc456185fd16761f3a4
VIVIAN NJAU <viviannjau@users.noreply.github.com>
2023-05-21 16:08:15 +0200

    index

--- server/index.js
+++ server/index.js
@@ -4,3 +4,10 @@ import cors from cors;
 
 dotenv.config();
 
+const app = express();
+app.use(cors());
+app.use(express.json( {limig: "50mb" }))
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Hello world"})
+})
